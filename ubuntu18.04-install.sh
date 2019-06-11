b2d6020e135b554a93e5016fc77d2a0f65e74577
eugen1j <36046510+eugen1j@users.noreply.github.com>
2019-06-11 13:46:00 +0300

    Update ubuntu18.04-install.sh

--- ubuntu18.04-install.sh
+++ ubuntu18.04-install.sh
@@ -1,5 +1,5 @@
 apt-get update
-apt-get upgrade
+apt-get upgrade -y
 apt-get install cmake gitlab-runner tmux software-properties-common nano make gcc g++ tcl golang php php7.2-gd php7.2-xml php7.2-dom php7.2-zip php7.2-fpm php7.2-mysql php7.2-pgsql php7.2-cli php7.2-json php7.2-common php7.2-mbstring php7.2-opcache php7.2-readline php7.2-curl certbot python-certbot-nginx redis composer python3-pip docker docker-compose ansible -y
 sudo add-apt-repository ppa:deadsnakes/ppa -y
 sudo apt-get update
@@ -11,10 +11,14 @@ apt-get update
 apt-get install postgresql-11 -y
 
 
-read -s -p "Postgres password: " PG_PASS
+stty -echo
+printf "Password: "
+read PG_PASS
+stty echo
+printf "\n"
 su - postgres -c "psql -U postgres -d postgres -c \"alter user postgres with password '$PG_PASS';\""
 
-apt-get autoremove
+apt-get autoremove -y
 
 service redis restart
 service nginx restart
