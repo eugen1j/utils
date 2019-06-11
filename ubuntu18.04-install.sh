d807932d70914c2a5edb5c33cce634ee0d1ca6ff
eugen1j <36046510+eugen1j@users.noreply.github.com>
2019-06-11 13:30:32 +0300

    Update ubuntu18.04-install.sh

--- ubuntu18.04-install.sh
+++ ubuntu18.04-install.sh
@@ -1,6 +1,6 @@
 apt-get update
 apt-get upgrade
-apt-get install cmake gitlab-runner tmux software-properties-common nano make gcc g++ tcl golang php php7.2-gd php7.2-xml php7.2-dom php7.2-zip php7.2-fpm php7.2-mysql php7.2-pgsql php7.2-cli php7.2-json php7.2-common php7.2-mbstring php7.2-opcache php7.2-readline php7.2-curl certbot python-certbot-nginx redis composer python3-pip -y
+apt-get install cmake gitlab-runner tmux software-properties-common nano make gcc g++ tcl golang php php7.2-gd php7.2-xml php7.2-dom php7.2-zip php7.2-fpm php7.2-mysql php7.2-pgsql php7.2-cli php7.2-json php7.2-common php7.2-mbstring php7.2-opcache php7.2-readline php7.2-curl certbot python-certbot-nginx redis composer python3-pip docker docker-compose ansible -y
 sudo add-apt-repository ppa:deadsnakes/ppa -y
 sudo apt-get update
 sudo apt-get install python3.7 -y
